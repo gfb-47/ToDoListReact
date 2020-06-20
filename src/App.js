67bea05a6fc7085e81ac0ab34d4ff23b3ba0d32d
gfb-47 <gabrieelfbrito@gmail.com>
2020-06-20 16:18:31 -0300

    ready, no styling yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TodoList from './components/TodoList';
+import TodoList from './components/TodoList'; 
 
 class App extends Component{
   render() {
